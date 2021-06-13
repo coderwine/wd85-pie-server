22e31edeb34697100585251871f3cc23dd656ed4
Eric <ejwinebrenner@gmail.com>
2021-06-13 08:00:04 -0400

    heroku connection

--- db.js
+++ db.js
@@ -3,6 +3,7 @@ const { Sequelize } = require('sequelize');
 
 // const db = new Sequelize(process.env.DB_CONNECTION_STRING);
 
+// For Sequelize v6 Heroku Connection
 const db = new Sequelize(process.env.DB_CONNECTION_STRING, {  // HEROKU DEPLOYMENT
     dialect: 'postgres',
     ssl: process.env.ENVIRONMENT === 'production'
